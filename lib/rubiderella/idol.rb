fd8bebb5eec84630e8a7512a18e56b737c74834f
hikko624 <hikko624@gmail.com>
2020-07-26 18:18:19 +0900

    update gemspec and ruby-version

---
+++ lib/rubiderella/idol.rb
@@ -0,0 +1,60 @@
+module Rubiderella
+  class Idol
+    attr_reader :name, :type, :age, :birthday, :sign, :blood, :height, :weight, :bust, :waist, :hip, :handed, :from,
+                :favorite, :cv
+    @config = nil
+    @all = nil
+
+    def initialize(args)
+      @name = args[:name]
+      @type = args[:type]
+      @age = args[:age]
+      @birthday = args[:birthday]
+      @sign = args[:sign]
+      @blood = args[:blood]
+      @height = args[:height]
+      @weight = args[:weight]
+      @bust = args[:bust]
+      @waist = args[:waist]
+      @hip = args[:hip]
+      @handed = args[:handed]
+      @from = args[:from]
+      @favorite = args[:favorite]
+      @cv = args[:cv]
+    end
+
+    class UnknownIdolError < StandardError; end
+
+    class << self
+      def config
+        @config = Dir.glob("#{File.dirname(__FILE__ )}/../../config/idols/*.yml").each_with_object({}) do |file, idols|
+          idols.merge!(YAML.load_file(file))
+        end.deep_symbolize_keys
+      end
+
+      def names
+        config.keys
+      end
+
+      def valid?(idol_name)
+        names.include?(idol_name)
+      end
+
+      def all
+        @all = config.map do |key, value|
+          value[:key] = key
+          new(**value)
+        end
+      end
+
+      def find_by_name(name)
+        begin
+          raise UnknownIdolError unless valid?(name)
+          new(@config[name])
+        rescue => e
+          "#{e}, Unknown Idol: #{name}"
+        end
+      end
+    end
+  end
+end
--- rubiderella.gemspec
+++ rubiderella.gemspec
@@ -27,4 +27,5 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'activesupport'
 
   spec.add_development_dependency 'pry'
+  spec.add_development_dependency 'pry-byebug'
 end
