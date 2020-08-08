c64d9f0243dbe42de7531b0abc969d3fd89fe295
hikko624 <hikko624@gmail.com>
2020-08-08 19:00:05 +0900

    fix by rubocop

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in rubiderella.gemspec
 gemspec
 
-gem "rake", "~> 12.0"
-gem "rspec", "~> 3.0"
+gem 'rake', '~> 12.0'
+gem 'rspec', '~> 3.0'
 
 gem 'activesupport', '< 6.0.0' if Gem::Version.create(RUBY_VERSION) < Gem::Version.create('2.5.0')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-if ENV["CI"] && ENV["CODECOV_TOKEN"]
+if ENV['CI'] && ENV['CODECOV_TOKEN']
   require 'simplecov'
   require 'codecov'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
@@ -9,14 +9,14 @@ if ENV["CI"] && ENV["CODECOV_TOKEN"]
   end
 end
 
-require "bundler/setup"
-require "rubiderella"
-require "rspec"
+require 'bundler/setup'
+require 'rubiderella'
+require 'rspec'
 require 'rspec-parameterized'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
